420aaf565e8e9761bb05a7eca604e95eec93027f
Alejandro Olarte Torres <50520017+AlejandroTorres33@users.noreply.github.com>
2021-11-22 17:58:43 -0500

    Form para insertar

--- src/components/Nosotros/Datos2.jsx
+++ src/components/Nosotros/Datos2.jsx
@@ -3,31 +3,70 @@ import React, { Component } from "react";
 class Datos2 extends Component {
   constructor(props) {
     super(props);
-    this.state = { datos: [] };
+    this.state = {
+      id:"",
+      nombre:"",
+      descripcion:"",
+      imagen:"",
+      restaurante_id:"",
+    };
   }
 
-  fonSubmit(e) {
-    e.preventDefault();
-    var title = this.title;
-    alert(title);
+
+  syncIdCambios(id){
+    this.setState({
+      id: id
+    })
+  }
+  syncNombreCambios(nombre){
+    this.setState({
+      nombre: nombre
+    })
+  }
+  syncDescripcionCambios(descripcion){
+    this.setState({
+      descripcion: descripcion
+    })
+  }
+  syncImagenCambios(imagen){
+    this.setState({
+      imagen: imagen
+    })
+  }
+  syncRestCambios(restaurante_id){
+    this.setState({
+      restaurante_id: restaurante_id
+    })
+  }
+  insertarEmpleado(id) {
+    fetch("http://localhost:16371/api/empleado/" + id, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        alert("dato eliminado");
+      });
+  }
+  submitForm(){
+    console.log(this.id);
+    console.log(this.nombre);
   }
 
-  render() {
-    return this.state.datos.map((data) => {
-      return (
+  render(){
+    return (
         <>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde consequuntur suscipit omnis adipisci consequatur voluptas, ut officia vero nemo iusto mollitia blanditiis possimus, reprehenderit esse sapiente eveniet cum debitis voluptates.</p>
-          <form className="form">
-            <input type="text" className="form-control" ref={(c) => this.title = c} name="title" />
-            <input type="text" className="form-control" ref={(c) => this.title = c} name="title" />
-            <input type="text" className="form-control" ref={(c) => this.title = c} name="title" />
-            <input type="text" className="form-control" ref={(c) => this.title = c} name="title" />
-          </form>
-          <button type="button" onClick={this.onSubmit} className="btn">Save</button>
+        <form>
+          <input onChange={(ev=>{ this.syncIdCambios(ev.target.value) })} type="number" value={this.state.id} placeholder="ID"/>
+          <input onChange={(ev=>{ this.syncNombreCambios(ev.target.value) })} type="text" value={this.state.nombre} placeholder="Nombre"/>
+          <input onChange={(ev=>{ this.syncDescripcionCambios(ev.target.value) })} type="text" value={this.state.descripcion} placeholder="Descripción"/>
+          <input onChange={(ev=>{ this.syncImagenCambios(ev.target.value) })} type="text" value={this.state.imagen} placeholder="Imagen"/>
+          <input onChange={(ev=>{ this.syncRestCambios(ev.target.value) })} type="number" value={this.state.restaurante_id} placeholder="Restaurante_ID"/>
+          <div>
+            <input onClick={()=>this.submitForm()} type="submit" value="Insertar"/>
+          </div>
+      </form>
         </>
-      );
-    });
+    );
   }
 }
-
 export default Datos2;
